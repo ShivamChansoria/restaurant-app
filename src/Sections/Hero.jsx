81171f8c4ebfbd76458c4b618410b1d5d5fd0671
ShivamChansoria <chansoriyashivam@gmail.com>
2024-04-20 17:09:09 +0530

    some-minor-fixes

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -5,25 +5,25 @@ const Hero = () => {
     <div className="hero  hero-contentmin-h- bg-base-200 flex flex-col ">
       <div className="carousel carousel-center p-4 space-x-4 bg-neutral rounded-box mt-9 mb-11">
         <div className="carousel-item">
-          <img src="./images/thali.png" height={100} width={700} className="rounded-box" />
+          <img src="./thali.png" height={100} width={700} className="rounded-box" />
         </div>
         <div className="carousel-item">
-          <img src="./images/chinese-1.jpg"height={100} width={700}  className="rounded-box" />
+          <img src="./chinese-1.jpg"height={100} width={700}  className="rounded-box" />
         </div>
         <div className="carousel-item">
-          <img src="./images/chinese-2.jpg"height={100} width={700}  className="rounded-box" />
+          <img src="./chinese-2.jpg"height={100} width={700}  className="rounded-box" />
         </div>
         <div className="carousel-item">
-          <img src="./images/snacks-1.jpg"height={100} width={700}  className="rounded-box" />
+          <img src="./snacks-1.jpg"height={100} width={700}  className="rounded-box" />
         </div>
         <div className="carousel-item">
-          <img src="./images/dinner-4.jpeg" height={100} width={700} className="rounded-box" />
+          <img src="./dinner-4.jpeg" height={100} width={700} className="rounded-box" />
         </div>
         <div className="carousel-item">
-          <img src="./images/dinner-13jpg.jpeg"height={100} width={700}  className="rounded-box" />
+          <img src="./dinner-13jpg.jpeg"height={100} width={700}  className="rounded-box" />
         </div>
         <div className="carousel-item">
-          <img src="./images/dinner-1.jpg"height={100} width={700}  className="rounded-box" />
+          <img src="./dinner-1.jpg"height={100} width={700}  className="rounded-box" />
         </div>
       </div>
       <h1 className="hero-content text-9xl min-[520px]:text-8xl font-bold tracking-wide mb-8 ">
